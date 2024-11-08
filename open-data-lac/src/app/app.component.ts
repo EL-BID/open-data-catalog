ccd8ac0a274d0511684c72be50afa64e5d0a2527
Marycielo PB <mparionabernaola@gmail.com>
2024-11-08 17:22:35 -0500

    implement homepage, routerlink, add icons and logos

--- open-data-lac/src/app/app.component.ts
+++ open-data-lac/src/app/app.component.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { RouterLink, RouterModule, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [CommonModule, RouterModule, RouterOutlet, RouterLink, RouterLinkActive, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]  // Asegúrate de añadir esto si usas Web Components
 })
 export class AppComponent {
   title = 'open-data-lac';
--- open-data-lac/src/app/app.routes.ts
+++ open-data-lac/src/app/app.routes.ts
@@ -6,6 +6,7 @@ import { DatasetDetailComponent } from './dataset-detail/dataset-detail.componen
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'dataset-catalog', component: DatasetCatalogComponent },
+  { path: 'browse', redirectTo: 'dataset-catalog', pathMatch: 'full' },
   { path: 'dataset/:category/:filename', component: DatasetDetailComponent },
   // Agrega más rutas según tus necesidades
 ];
