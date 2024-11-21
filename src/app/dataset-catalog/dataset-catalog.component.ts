f040a36f40729f58fef55051a5d04f42f0486eeb
Marycielo PB <mparionabernaola@gmail.com>
2024-11-21 09:10:16 -0500

    clean code

--- src/app/dataset-catalog/dataset-catalog.component.ts
+++ src/app/dataset-catalog/dataset-catalog.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectorRef, Component, AfterViewInit, OnInit  } from '@angular/core';
+import { ChangeDetectorRef, Component, AfterViewInit  } from '@angular/core';
 import { SearchBarComponent } from "../search-bar/search-bar.component";
 import { FiltersComponent } from "../filters/filters.component";
 import { SearchResultsComponent } from '../search-results/search-results.component';
@@ -13,11 +13,11 @@ import { ResultsHeaderComponent } from '../results-header/results-header.compone
   styleUrls: ['./dataset-catalog.component.scss']
 })
 export class DatasetCatalogComponent implements AfterViewInit {
-  searchTerm: string = '';  // Término de búsqueda inicial
-  sortBy: string = 'date'; // Criterio de ordenamiento inicial
-  filters: any = {};  // Filtros seleccionados, como por ejemplo, categoría o tipo de datos
-  resultCount: number = 0; // Nueva propiedad para el conteo de resultados
-  noResults: boolean = false; // Nueva propiedad para indicar si no hay resultados
+  searchTerm: string = '';
+  sortBy: string = 'date';
+  filters: any = {};
+  resultCount: number = 0;
+  noResults: boolean = false;
   datasetPath: string | null = null;
   currentPage = 0;
   rowsPerPage = 10;
@@ -25,55 +25,31 @@ export class DatasetCatalogComponent implements AfterViewInit {
   constructor(private cdr: ChangeDetectorRef) { }
 
   ngAfterViewInit(): void {
-    this.cdr.detectChanges();  // Realiza la detección de cambios después de que la vista esté completamente inicializada
-  }
-
-  // Este método recibe el término de búsqueda desde el componente hijo SearchBar
-  onSearch(searchTerm: string): void {
-    console.log('Término de búsqueda recibido en DatasetCatalog:', searchTerm);  // Depuración
-    this.searchTerm = searchTerm;
-    // Fuerza la detección de cambios
     this.cdr.detectChanges();
   }
 
-  // Método para cambiar el criterio de ordenamiento en SearchResults
+  onSearch(searchTerm: string): void {
+    console.log('Término de búsqueda recibido en DatasetCatalog:', searchTerm);
+    this.searchTerm = searchTerm;
+    this.cdr.detectChanges();
+  }
+
   onSortChanged(sortBy: string): void {
-    console.log('Criterio de ordenamiento cambiado a:', sortBy);  // Verifica el valor recibido
+    console.log('Criterio de ordenamiento cambiado a:', sortBy);
     this.sortBy = sortBy;
   }
 
-  // Método para manejar los cambios en los filtros
   onFiltersChanged(filters: any): void {
-    console.log('Filtros cambiados a:', filters);  // Verifica los filtros recibidos
+    console.log('Filtros cambiados a:', filters);
     this.filters = filters;
   }
 
-   // Método para actualizar el conteo de resultados desde SearchResultsComponent
   updateResultCount(count: number): void {
     this.resultCount = count;
-    this.cdr.detectChanges();  // Fuerza la detección de cambios
+    this.cdr.detectChanges();
   }
 
-  // Método para actualizar la propiedad noResults desde SearchResultsComponent
   updateNoResults(noResults: boolean): void {
     this.noResults = noResults;
   }
-
-  // get paginatedDataset() {
-  //   const start = this.currentPage * this.rowsPerPage;
-  //   const end = start + this.rowsPerPage;
-  //   return this.datasetContent.slice(start, end);
-  // }
-
-  // nextPage() {
-  //   if ((this.currentPage + 1) * this.rowsPerPage < this.datasetContent.length) {
-  //     this.currentPage++;
-  //   }
-  // }
-
-  // previousPage() {
-  //   if (this.currentPage > 0) {
-  //     this.currentPage--;
-  //   }
-  // }
 }
--- src/app/search-results/search-results.component.ts
+++ src/app/search-results/search-results.component.ts
@@ -30,7 +30,7 @@ export class SearchResultsComponent implements OnChanges, AfterViewInit {
     this.dataService.getMetadata().subscribe(data => {
       this.datasets = data;
       this.applyFilters();
-      setTimeout(() => this.checkTruncation(), 0); // Espera a que las vistas se actualicen
+      setTimeout(() => this.checkTruncation(), 0);
     });
   }
 
@@ -41,7 +41,6 @@ export class SearchResultsComponent implements OnChanges, AfterViewInit {
   }
 
   ngAfterViewInit(): void {
-    // Lógica que se ejecuta después de que las vistas secundarias estén inicializadas.
     this.checkTruncation();
   }
 
@@ -50,16 +49,13 @@ export class SearchResultsComponent implements OnChanges, AfterViewInit {
 
     this.filteredDatasets = this.datasets.filter(dataset => {
       const matchSearchTerm = dataset.title.toLowerCase().includes(this.searchTerm.toLowerCase());
-       // Filtrar por temas (temas pueden ser un string o un array de strings)
-    const matchTopic = topics.length === 0 || topics.some((top: string) => {
-      if (Array.isArray(dataset.theme)) {
-        // Si dataset.theme es un array, verificar si contiene el tema
-        return dataset.theme.some((t: string) => t.toLowerCase() === top.toLowerCase());
-      } else {
-        // Si dataset.theme es un string, comparar directamente
-        return dataset.theme && dataset.theme.toLowerCase() === top.toLowerCase();
-      }
-    });
+      const matchTopic = topics.length === 0 || topics.some((top: string) => {
+        if (Array.isArray(dataset.theme)) {
+          return dataset.theme.some((t: string) => t.toLowerCase() === top.toLowerCase());
+        } else {
+          return dataset.theme && dataset.theme.toLowerCase() === top.toLowerCase();
+        }
+      });
       const matchCountries = countries.length === 0 || this.isMatchingSpatial(countries, dataset.spatial);
       const matchYear = years.length === 0 || years.some((year: string) => {
         const issuedYear = dataset.issued ? dataset.issued.split('-')[0] : '';
@@ -68,10 +64,8 @@ export class SearchResultsComponent implements OnChanges, AfterViewInit {
       const matchLanguage = languages.length === 0 || languages.some((lang: string) => dataset.language.toLowerCase() === lang.toLowerCase());
       const matchIdbKnowledge = idbKnowledges.length === 0 || idbKnowledges.some((knowledge: string) => {
         if (knowledge === "Tied research publication") {
-          // Si la opción "Tied research publication" está seleccionada, solo filtrar datasets cuyo "source" no sea vacío
           return dataset.source && dataset.source.trim() !== "";
         }
-        // Si no se ha seleccionado "Tied research publication", no filtra por el campo source
         return true;
       });
 
@@ -82,7 +76,7 @@ export class SearchResultsComponent implements OnChanges, AfterViewInit {
     this.resultCountChange.emit(this.filteredDatasets.length);
     this.noResultsChange.emit(this.filteredDatasets.length === 0);
 
-    setTimeout(() => this.checkTruncation(), 0); // Recalcula después de aplicar los filtros
+    setTimeout(() => this.checkTruncation(), 0);
   }
 
   private isMatchingSpatial(selectedCountries: string[], spatial: string | string[]): boolean {
@@ -112,7 +106,7 @@ export class SearchResultsComponent implements OnChanges, AfterViewInit {
       if (descriptionElement) {
         const isTruncated = descriptionElement.nativeElement.scrollHeight > descriptionElement.nativeElement.offsetHeight;
         this.showReadMoreButton[dataset.id] = isTruncated;
-        this.isDescriptionExpanded[dataset.id] = false;  // Inicialmente está colapsada
+        this.isDescriptionExpanded[dataset.id] = false;
       }
     });
   }
@@ -122,7 +116,6 @@ export class SearchResultsComponent implements OnChanges, AfterViewInit {
   }
 
   viewDataset(mydata_category: string, title_original: string, mydata_id?: string): void {
-    // Formatear la categoría y el título reemplazando caracteres no deseados por guiones y limitando a 50 caracteres
     const formattedCategory = mydata_category
       ? mydata_category.replace(/[^a-zA-Z0-9]+/g, '-').substring(0, 50)
       : '';
@@ -130,23 +123,17 @@ export class SearchResultsComponent implements OnChanges, AfterViewInit {
       .replace(/[^a-zA-Z0-9]+/g, '-')
       .substring(0, 50);
 
-    // Construcción de la ruta en función de los parámetros disponibles
     let route = '';
     if (formattedCategory && mydata_id) {
-      // Si tenemos category y mydata_id
       route = `/${formattedCategory}/${formattedTitle}/${mydata_id}`;
     } else if (formattedCategory) {
-      // Si solo tenemos category
       route = `/${formattedCategory}/${formattedTitle}`;
     } else if (mydata_id) {
-      // Si solo tenemos mydata_id
       route = `/dataset/${formattedTitle}/${mydata_id}`;
     } else {
-      // Si solo tenemos title
       route = `/dataset/${formattedTitle}`;
     }
 
-    // Navegar a la ruta generada
     this.router.navigate([route]);
   }
 
