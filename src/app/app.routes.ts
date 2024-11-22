98904d0d545f175a27369a227bf61880066df8ea
Marycielo PB <mparionabernaola@gmail.com>
2024-11-22 14:19:42 -0500

    implement dynamic breadcrumb

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,19 +4,20 @@ import { DatasetCatalogComponent } from './dataset-catalog/dataset-catalog.compo
 import { DatasetDetailComponent } from './dataset-detail/dataset-detail.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'browse', component: DatasetCatalogComponent },
+  { path: '', component: HomeComponent, data: { breadcrumb: null } },
+  { path: 'browse', component: DatasetCatalogComponent, data: { breadcrumb: 'Dataset Catalog' } },
   { path: 'dataset-catalog', redirectTo: 'browse', pathMatch: 'full' },
 
-  { path: 'dataset/:title_original', component: DatasetDetailComponent },
+  { path: 'dataset/:title_original', component: DatasetDetailComponent, data: { breadcrumb: 'Dataset Detail' } },
   { path: 'dataset/:title_original/about-data', redirectTo: 'dataset/:title_original', pathMatch: 'full' },
 
-  { path: 'dataset/:title_original/:mydata_id', component: DatasetDetailComponent },
+  { path: 'dataset/:title_original/:mydata_id', component: DatasetDetailComponent, data: { breadcrumb: 'Dataset Detail' } },
   { path: 'dataset/:title_original/:mydata_id/about-data', redirectTo: 'dataset/:title_original/:mydata_id', pathMatch: 'full' },
 
-  { path: ':mydata_category/:title_original', component: DatasetDetailComponent },
+  { path: ':mydata_category/:title_original', component: DatasetDetailComponent, data: { breadcrumb: 'Dataset Detail' } },
   { path: ':mydata_category/:title_original/about-data', redirectTo: ':mydata_category/:title_original', pathMatch: 'full' },
 
-  { path: ':mydata_category/:title_original/:mydata_id', component: DatasetDetailComponent },
+  { path: ':mydata_category/:title_original/:mydata_id', component: DatasetDetailComponent, data: { breadcrumb: 'Dataset Detail' } },
   { path: ':mydata_category/:title_original/:mydata_id/about-data', redirectTo: ':mydata_category/:title_original/:mydata_id', pathMatch: 'full' },
+
 ];
