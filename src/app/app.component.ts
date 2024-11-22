073f51e613072f57407fddb842d60ff2258603dd
Marycielo PB <mparionabernaola@gmail.com>
2024-11-21 21:13:13 -0500

    clean code, remove comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,7 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA, OnInit } from '@angular/core';
-import { Router, NavigationEnd } from '@angular/router';
-import { filter, pairwise } from 'rxjs/operators'; // pairwise para rastrear la URL anterior
+import { Router, RouterModule, RouterOutlet, NavigationEnd } from '@angular/router';
+import { filter, pairwise } from 'rxjs/operators';
 import { CommonModule } from '@angular/common';
-import { RouterModule, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 
@@ -26,17 +25,14 @@ export class AppComponent implements OnInit {
   constructor(private router: Router) {}
 
   ngOnInit() {
-    // Suscribirse a eventos de navegación y distinguir entre cambios de ruta y query params
     this.router.events
       .pipe(
-        filter(event => event instanceof NavigationEnd), // Solo NavigationEnd
-        pairwise() // Obtener la URL anterior y la actual
+        filter(event => event instanceof NavigationEnd),
+        pairwise()
       )
       .subscribe(([prev, curr]: [NavigationEnd, NavigationEnd]) => {
         const prevUrl = new URL(prev.urlAfterRedirects, window.location.origin);
         const currUrl = new URL(curr.urlAfterRedirects, window.location.origin);
-
-        // Condición: Desplazarse hacia arriba solo si el pathname cambia
         if (prevUrl.pathname !== currUrl.pathname) {
           window.scrollTo({
             top: 0,
--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,17 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
-import { routes } from './app/app.routes'; // Importar las rutas desde app.routes.ts
+import { routes } from './app/app.routes';
 
-// Configuración del arranque de la aplicación, ahora con las rutas
 bootstrapApplication(AppComponent, {
   providers: [
-    ...appConfig.providers, // Si tienes otras configuraciones en appConfig
-    provideRouter(routes)   // Añadir el router con las rutas definidas
+    ...appConfig.providers,
+    provideRouter(routes)
   ]
 })
   .catch((err) => console.error(err));
 
-// Deshabilitar la restauración del desplazamiento por defecto en el navegador
 if ('scrollRestoration' in history) {
-  history.scrollRestoration = 'manual';  // Siempre iniciar desde arriba
+  history.scrollRestoration = 'manual';
 }
 
