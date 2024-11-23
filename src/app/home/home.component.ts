db7cafcc837b2880411b76d943dde911b74ba3f8
Marycielo PB <mparionabernaola@gmail.com>
2024-11-22 23:34:38 -0500

    add some fixes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterModule, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterModule} from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink, RouterModule, RouterLinkActive, RouterOutlet],
+  imports: [RouterLink, RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
