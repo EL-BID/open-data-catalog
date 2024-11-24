f7dc3182b6f39605eb971dcdf743101a89eb7c64
Marycielo PB <mparionabernaola@gmail.com>
2024-11-23 19:37:16 -0500

    implement automatic list of liked research dataset and reuse the route functionality

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterLink, RouterModule} from '@angular/router';
 import { DataService } from '../data.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink, RouterModule],
+  imports: [RouterLink, RouterModule, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
@@ -15,11 +16,21 @@ export class HomeComponent implements OnInit {
     linkedResearchDatasets: '0',
   };
 
+  recentDatasets: any[] = [];
+
   constructor(private dataService: DataService) {}
 
   ngOnInit(): void {
     this.dataService.getMetadata().subscribe(metadata => {
       this.counts = this.dataService.getFormattedCounts(metadata);
     });
+
+    this.dataService.getRecentLinkedResearchDatasets().subscribe(datasets => {
+      this.recentDatasets = datasets;
+    });
+  }
+  // Generar la URL del dataset usando el servicio
+  generateDatasetUrl(dataset: any): string {
+    return this.dataService.generateDatasetRoute(dataset.mydata_category, dataset.title_original, dataset.mydata_id);
   }
 }
