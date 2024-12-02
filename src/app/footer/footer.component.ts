26c72a031de7fb6664eeb87024fdda2fdf2e6a0c
Marycielo PB <mparionabernaola@gmail.com>
2024-12-02 10:54:06 -0500

    fix some styles and add social icons

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faLinkedin, faFacebook, faXTwitter, faInstagram, faYoutube, faFlickr} from '@fortawesome/free-brands-svg-icons';
+import { faPodcast } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
-
+  faLinkedin = faLinkedin;
+  faFacebook = faFacebook;
+  faXTwitter = faXTwitter;
+  faInstagram = faInstagram;
+  faYoutube= faYoutube;
+  faFlickr = faFlickr;
+  faPodcast = faPodcast;
 }
