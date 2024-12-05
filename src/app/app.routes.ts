9c86c665e41a567566739971f99e3e3ad0d8e302
Marycielo PB <mparionabernaola@gmail.com>
2024-12-04 21:09:27 -0500

    improve load dataset metadata and reduce routes structures, implement dynamic title

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,17 +4,13 @@ import { DatasetCatalogComponent } from './dataset-catalog/dataset-catalog.compo
 import { DatasetDetailComponent } from './dataset-detail/dataset-detail.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
-
 export const routes: Routes = [
   { path: '', component: HomeComponent, data: { breadcrumb: 'Open Data LAC', title: 'IDB | Open Data LAC' } },
   { path: 'home', component: HomeComponent, data: { breadcrumb: 'Open Data LAC', title: 'IDB | Open Data LAC' } },
   { path: 'browse', component: DatasetCatalogComponent, data: { breadcrumb: 'Dataset Catalog', title: 'IDB | Dataset Catalog' } },
   { path: 'dataset-catalog', redirectTo: 'browse', pathMatch: 'full' },
-
-  { path: ':mydata_category/:title_original', component: DatasetDetailComponent, data: { breadcrumb: 'Dataset Detail', title: 'IDB | Dataset' } },
-
-  { path: ':mydata_category/:title_original/:section', redirectTo: ':mydata_category/:title_original', pathMatch: 'full' },
-  { path: ':mydata_category/:title_original/:mydata_id/:section', redirectTo: ':mydata_category/:title_original', pathMatch: 'full' },
+  { path: ':mydata_category/:title_original', component: DatasetDetailComponent, data: { breadcrumb: 'Dataset Detail' } },
+  { path: ':mydata_category/:title_original/:mydata_id', redirectTo: ':mydata_category/:title_original', pathMatch: 'full' },
+  { path: ':mydata_category/:title_original/:mydata_id/:section', redirectTo: ':mydata_category/:title_original', pathMatch: 'full'},
   { path: '**', component: NotFoundComponent, data: { breadcrumb: '404 Page not found', title: '404 | Page Not Found' } }
-
 ];
